d22973f022b28fb78e42a127e81cbf0f49073fb4
erkiinov <erkinovbavrom12@gmail.com>
2023-05-13 00:40:45 +0500

    vibrate

--- src/features/Timer.js
+++ src/features/Timer.js
@@ -1,27 +1,47 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Vibration } from "react-native";
+import { ProgressBar } from "react-native-paper";
 import { Countdown } from "../components/CountDown";
 import { RoundedButton } from "../components/Button";
 import { spacing } from "../utils/sizes";
 import { colors } from "../utils/colors";
 
+const ONE_SECOND_IN_MS = 1000;
+
+const pattern = [
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+  1 * ONE_SECOND_IN_MS,
+];
+
 export const Timer = ({ focusSubject }) => {
   const [isStarted, setIsStarted] = useState(false);
-
+  const [progress, setProgress] = useState(1);
   return (
     <View style={styles.container}>
       <View style={styles.countdown}>
         <Countdown
           isPaused={!isStarted}
-          onProgress={() => {}}
+          onProgress={setProgress}
           onEnd={() => {
-            Vibration.vibrate();
+            Vibration.vibrate(pattern);
           }}
         />
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Focusing on: </Text>
           <Text style={styles.task}>{focusSubject}</Text>
         </View>
+      </View>
+
+      <View style={styles.ProgressBar}>
+        <ProgressBar
+          progress={progress}
+          color={colors.progressBar}
+          style={{ height: spacing.sm }}
+        />
       </View>
 
       <View style={styles.buttonWrapper}>
@@ -45,9 +65,10 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   buttonWrapper: {
-    flex: 0.3,
+    flex: 0.4,
     flexDirection: "row",
     padding: 15,
+    paddingTop: 50,
     justifyContent: "center",
     alignItems: "center",
   },
@@ -62,5 +83,9 @@ const styles = StyleSheet.create({
   },
   titleContainer: {
     paddingTop: spacing.xxl,
+  },
+  ProgressBar: {
+    paddingTop: spacing.sm,
+    flex: 0.1,
   },
 });
--- src/utils/colors.js
+++ src/utils/colors.js
@@ -1,4 +1,6 @@
 export const colors = {
   white: "#fff",
   darkBlue: "#252250",
+  lightBlue: "#a7cbd9",
+  progressBar: "#5e84e2",
 };
